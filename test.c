b3adadd021d5fc6507a4e9f52836ec837a2c7476
guypbi <guypazbenitzhak@gmail.com>
2023-01-21 00:05:42 +0200

    backup

--- test.c
+++ test.c
@@ -3,6 +3,14 @@
 //#include "units.h"
 
 int main(int argc, char** argv) {
-    printf("hi\n");
-    initMemory("memin.txt","memout.txt");
+    char name [5];
+    name[0] = 'A';
+    name[1] = 'D';
+    name[2] = 'D';
+    name[3] = (char) 55;
+    name[4] = '\0';
+    char* hi;
+    hi = name;
+    FILE* memFile = fopen("hello.txt","w");
+    fprintf(memFile,"hi: %s\n",hi);
 }
